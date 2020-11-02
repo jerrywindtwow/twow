846bc904bb768d72bf37b7323f3bde9a54934c29
jerrywindtwow <jerrywind@hotmail.com>
2020-11-02 19:48:32 +0900

    revise code according to PEP8

--- py_excercise/connectDB.py
+++ py_excercise/connectDB.py
@@ -1,28 +1,27 @@
 import sys
 import psycopg2
 import pandas as pd
-import numpy as np
-import separator as sp
+import py_excercise.separator as sp
 from pathlib import Path
 import shutil
 
-sql_query_data_count = "SELECT schemaname as schema_name,relname as table_name,n_live_tup as count " \
-                       "FROM pg_stat_user_tables ORDER BY n_live_tup DESC"
-filename = "C:\Tools\programing\python\\temp\output.xlsx"
 
-"""
+sql_query_data_count = "SELECT schemaname as schema_name, relname as table_name,n_live_tup as count " \
+                       "FROM pg_stat_user_tables ORDER BY n_live_tup DESC"
+filename = "C:\\Tools\\programing\\python\\temp\\output.xlsx"
+
 # get column quantity from matched schema & table name
-"""
+
+
 def get_size(param_excel_row, rows):
     for row in rows:
-        if param_excel_row.iat[0,0] == row[0] and param_excel_row.iat[0,1] == row[1]:
+        if param_excel_row.iat[0, 0] == row[0] and param_excel_row.iat[0, 1] == row[1]:
             return row[2]
 
     return None
 
-'''
-get table count from db
-'''
+
+# get table count from db
 def get_table_count(param_excel, param_rs_sizes):
     target_index_count = param_excel.shape[0]
     for index in range(0, target_index_count):
@@ -38,10 +37,12 @@ def get_table_count(param_excel, param_rs_sizes):
 
     return param_excel
 
-#connect to DB
+# connect to DB
+
+
 try:
     conn = psycopg2.connect(database="firstdb", user="jerry", password="zaq12wsx", host="localhost", port="5432")
-except Exception(BaseException) :
+except psycopg2.Error as err:
     print("failed to connect to DB.")
     sys.exit(1)
 
@@ -49,8 +50,12 @@ print("Opened database successfully")
 
 # fetch data from DB
 cursor = conn.cursor()
+headers = [x[0] for x in cursor.description]
 cursor.execute(sql_query_data_count)
 result_sizes = cursor.fetchall()
+df = pd.DataFrame(result_sizes, columns=headers)
+print(df.head(5))
+
 # for row in table_sizes:
 #     print(row)
 # print("------------------- print table columns ----------------")
@@ -60,14 +65,14 @@ result_sizes = cursor.fetchall()
 
 # read excel
 print("\n---------- read target objects from excel -------------")
-excel = pd.read_excel(filename, sheet_name="Sheet1");
+excel = pd.read_excel(filename, sheet_name="Sheet1")
 # excel['size']=None
 # result_df = pd.DataFrame(index=[],columns=excel.columns)
 
 print("\n------------------- print excel filled with count ----------------")
 # excel = get_table_count(excel,result_sizes)
 
-sorted_tables = excel.sort_values(by=['type','size'], ascending=True)
+sorted_tables = excel.sort_values(by=['type', 'size'], ascending=True)
 print(sorted_tables)
 
 # delete root directory if exist
@@ -82,15 +87,15 @@ root_dir = root_dir.name
 print("\n------------------- print data filtered by type ----------------")
 group_num = 3
 type_num = 3
-sorted_tables['group']=group_num
+sorted_tables['group'] = group_num
 col_name = 'size'
 filename_suffix = '_tablelist.csv'
 
-for i_type in range(1,type_num+1):
+for i_type in range(1, type_num+1):
     # print("________________", str(i_type), "________________")
     df_every_group = sorted_tables.query('type==' + str(i_type))
     df_every_group = sp.separate_to_groups(df_every_group, col_name, group_num)
-    df_every_group = df_every_group[['schema', 'group', 'table_name', 'type','size','date']]
+    df_every_group = df_every_group[['schema', 'group', 'table_name', 'type', 'size', 'date']]
     print(df_every_group)
 
     group_indices = df_every_group.groupby('group').count().index.values
@@ -103,6 +108,6 @@ for i_type in range(1,type_num+1):
         for schema_index in schema_indices:
             filename = dir_name + '/' + schema_index + filename_suffix
             tmp_write_to = df_every_group.query('group=='+str(group_index)+' and schema=="'+schema_index+'"')
-            tmp_write_to.to_csv(filename, index=False,header=False,mode='a+')
+            tmp_write_to.to_csv(filename, index=False, header=False, mode='a+')
 
 conn.close()
--- py_excercise/separator.py
+++ py_excercise/separator.py
@@ -1,17 +1,18 @@
 import math
 import pandas as pd
 
+
 def separate_to_groups(par_df_first, par_col_name, par_group_num):
-    par_df=par_df_first
+    par_df = par_df_first
     cols = par_df_first.columns
     grouped = pd.DataFrame(index=[], columns=cols)
     str_where = 'group==' + str(par_group_num)
 
-    for group_id in range(1,par_group_num):
-        par_df=par_df.query(str_where)
+    for group_id in range(1, par_group_num):
+        par_df = par_df.query(str_where)
         data_sum = par_df[par_col_name].sum()
         amount = par_df.shape[0]
-        ave = math.ceil(data_sum / (par_group_num - group_id + 1 ))
+        ave = math.ceil(data_sum / (par_group_num - group_id + 1))
         # print(par_df[par_col_name])
         # print("avg=",ave)
 
@@ -19,29 +20,29 @@ def separate_to_groups(par_df_first, par_col_name, par_group_num):
         abs_diff = data_sum
         abs_diff_index = -1
 
-        for j in range(amount-1,-1,-1):
-            elem_df =  par_df[j:j+1]
+        for j in range(amount-1, -1, -1):
+            elem_df = par_df[j:j+1]
             elem_index = elem_df.index[0]
-            elem = par_df.at[elem_index,'size']
-            tmp = remain- elem
+            elem = par_df.at[elem_index, 'size']
+            tmp = remain - elem
 
-            if elem>=ave:
+            if elem >= ave:
                 par_df.at[elem_index, 'group'] = group_id
                 par_df_first.at[elem_index, 'group'] = group_id
                 grouped = grouped.append(elem_df)
                 abs_diff = data_sum
-                abs_diff_index=-1
+                abs_diff_index = -1
                 break
-            elif tmp>=0:
+            elif tmp >= 0:
                 remain = tmp
                 par_df.at[elem_index, 'group'] = group_id
                 par_df_first.at[elem_index, 'group'] = group_id
                 grouped = grouped.append(elem_df)
                 abs_diff = data_sum
-                abs_diff_index=-1
+                abs_diff_index = -1
             elif abs(tmp) < abs_diff:
-                    abs_diff = abs(tmp)
-                    abs_diff_index = elem_index
+                abs_diff = abs(tmp)
+                abs_diff_index = elem_index
 
         if remain>abs_diff:
             par_df.at[abs_diff_index, 'group'] = group_id
